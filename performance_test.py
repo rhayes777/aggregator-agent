e4db42b3176f77f608d74ee7a29b2c60c4b662e3
Richard <richard@rghsoftware.co.uk>
2025-11-28 11:35:58 +0000

    quick test

--- performance_test.py
+++ performance_test.py
@@ -1,9 +1,12 @@
 from pathlib import Path
 from csv import DictReader
+
+from aggregator_agent.image_agent import categorise
 from aggregator_agent.schema import LensFitAnalysis
 
 directory = Path(__file__).parent
 data_directory = directory / "data"
+initial_lens_model_directory = data_directory / "initial_lens_model"
 
 
 class GroundTruth(LensFitAnalysis):
@@ -13,9 +16,21 @@ class GroundTruth(LensFitAnalysis):
 
     id: str
 
+    @property
+    def image_path(self) -> Path:
+        """
+        The path to the test data. Assumed to be in data/initial_lens_model.
+        """
+        return (initial_lens_model_directory / self.id).with_suffix(".png")
+
 
 with open(directory / "image_analysis.csv") as f:
     ground_truths = [GroundTruth.model_validate(row) for row in DictReader(f)]
 
-for ground_truth in ground_truths:
-    print(ground_truth)
+ground_truth = ground_truths[0]
+
+print("Expected")
+print(ground_truth.model_dump_json(indent=2))
+
+print("\nActual")
+print(categorise(ground_truth.image_path).model_dump_json(indent=2))
