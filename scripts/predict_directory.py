b69416f0739d4eb5049d0e97799a69002d40a9a9
Richard <richard@rghsoftware.co.uk>
2025-11-28 12:28:48 +0000

    script for usage

---
+++ scripts/predict_directory.py
@@ -0,0 +1,28 @@
+import csv
+from argparse import ArgumentParser
+from pathlib import Path
+
+from aggregator_agent.image_agent import categorise
+
+parser = ArgumentParser("Read images from a directory and assess the quality of the lensing")
+
+parser.add_argument(
+    "directory",
+    type=Path,
+)
+parser.add_argument(
+    "--output",
+    type=Path,
+    default=None,
+)
+
+args = parser.parse_args()
+
+output_filename = args.output or args.directory.with_name(f"{args.directory.stem}_categorised.csv")
+
+with output_filename.open("w") as f:
+    writer = csv.writer(f)
+    writer.writerow(["id", "category", "description"])
+    for path in args.directory.iterdir():
+        result = categorise(path)
+        writer.writerow([path.stem, result.category, result.description])
